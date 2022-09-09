1f87ddb03ce5bfee192d17da2529db12dc730742
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2022-09-09 10:02:20 -0300

    feat: add classe de respositorio

---
+++ src/main/java/br/com/sigep/servicos/Repositorio.java
@@ -0,0 +1,11 @@
+package br.com.sigep.servicos;
+
+import java.util.List;
+
+public interface Repositorio<T> {
+    public T Abrir(Long id);
+    public boolean Salvar(T obj);
+    public boolean Atualizar(T obj);
+    public boolean Apagar(T obj);
+    public List<T> Buscar(T filtro);
+}
