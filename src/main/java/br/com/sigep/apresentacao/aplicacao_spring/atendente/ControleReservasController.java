12a07bf3e1ec74b531d303dd6bb92f1b65aa2a3e
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2022-10-05 08:33:41 -0300

    fix: corrigindo exibição do menu lateral

--- src/main/java/br/com/sigep/apresentacao/aplicacao_spring/atendente/ControleReservasController.java
+++ src/main/java/br/com/sigep/apresentacao/aplicacao_spring/atendente/ControleReservasController.java
@@ -3,8 +3,6 @@ package br.com.sigep.apresentacao.aplicacao_spring.atendente;
 import org.springframework.stereotype.Service;
 
 import br.com.sigep.apresentacao.aplicacao_spring.Controller;
-import javafx.event.Event;
-import javafx.fxml.FXML;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @Service
