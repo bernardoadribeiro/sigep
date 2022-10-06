dcb418c98845d79ec5a30ef8bcb7fe1a6c0bff64
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2022-10-06 15:45:16 -0300

    feat: preenchendo seletores de categoria e status

--- src/main/java/br/com/sigep/entidades/quarto/QuartoCategoria.java
+++ src/main/java/br/com/sigep/entidades/quarto/QuartoCategoria.java
@@ -1,7 +1,20 @@
 package br.com.sigep.entidades.quarto;
 
 public enum QuartoCategoria {
-    Junior,
-    Top,
-    Master
+    Junior("Júnior"),
+    Top("Top"),
+    Master("Master");
+
+    private String descricao;
+
+    private QuartoCategoria(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
+
+    
 }
