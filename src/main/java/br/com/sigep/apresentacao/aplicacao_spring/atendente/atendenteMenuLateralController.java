c4db3b7825f3fefe9de6ea4335fb0924a550b1c9
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2022-10-06 00:38:56 -0300

    feat: implementa transicao entre paginas de Controle e Quartos

--- src/main/java/br/com/sigep/apresentacao/aplicacao_spring/atendente/atendenteMenuLateralController.java
+++ src/main/java/br/com/sigep/apresentacao/aplicacao_spring/atendente/atendenteMenuLateralController.java
@@ -3,25 +3,29 @@ package br.com.sigep.apresentacao.aplicacao_spring.atendente;
 import org.springframework.stereotype.Service;
 
 import br.com.sigep.apresentacao.aplicacao_spring.Controller;
+import br.com.sigep.apresentacao.aplicacao_spring.login.LoginController;
 import br.com.sigep.apresentacao.aplicacao_spring.quarto.QuartosController;
-import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @Service
 @FxmlView("atendenteMenu.fxml")
 public class AtendenteMenuLateralController extends Controller{
 
-    @FXML
-    BorderPane atendenteControle;
-
     public AtendenteMenuLateralController() {
     }
+
+    @FXML
+    AnchorPane menuLateral; // Nome e tipo do componente que esta no fxml como componente pai (parent).
+
+    @FXML private Label labelApelido;
+    @FXML private Label labelCargo;
 
     @FXML
     public void actionLogout(Event e){
@@ -29,18 +33,18 @@ public class AtendenteMenuLateralController extends Controller{
         alert.showAndWait();
 
         if (alert.getResult() == ButtonType.YES) {
-            Platform.exit();
+            carregarScene(getAncora(), LoginController.class);
         }
     }
     
     @FXML
     public void abrirPagControleReservas(Event e){
-        
+        carregarScene(menuLateral, AtendenteControleReservasController.class);
     }
 
     @FXML
     public void abrirPagQuartos(Event e){
-        carregarScene(atendenteControle, QuartosController.class);
+        carregarScene(menuLateral, QuartosController.class);
 
     }
     
--- src/main/java/br/com/sigep/apresentacao/aplicacao_spring/login/LoginController.java
+++ src/main/java/br/com/sigep/apresentacao/aplicacao_spring/login/LoginController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Service;
 
 import br.com.sigep.apresentacao.aplicacao_spring.Controller;
 import br.com.sigep.apresentacao.aplicacao_spring.atendente.AtendenteControleReservasController;
+import br.com.sigep.apresentacao.aplicacao_spring.quarto.QuartosController;
 
 //import com.jfoenix.controls.JFXButton.ButtonType;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import net.rgielen.fxweaver.core.FxmlView;
 
@@ -42,7 +43,7 @@ public class LoginController extends Controller {
         // if (alert.getResult() == ButtonType.OK){
         //     carregarScene(telaLogin, AtendenteControleReservasController.class);
         // }
-
+       
         String login = emailLogin.getText();
         String senha = passwordLogin.getText();
 
@@ -53,7 +54,7 @@ public class LoginController extends Controller {
             carregarScene(telaLogin, AtendenteControleReservasController.class); //substituir pelo controller do Dashboard quando for implementado
 
         } else if (login.equals("proprietario")){
-            carregarScene(telaLogin, AtendenteControleReservasController.class); //substituir pelo controller do Dashboard quando for implementado
+            carregarScene(telaLogin, QuartosController.class); //substituir pelo controller do Dashboard quando for implementado
 
         } else {
             Alert alert = new Alert(AlertType.ERROR, "Por favor, informe seu login corretamente! ", ButtonType.OK);
